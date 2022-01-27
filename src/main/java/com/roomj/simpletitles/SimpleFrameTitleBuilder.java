07c1ee4d046a8c94e97bd1394b67821374809e04
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2022-01-27 22:03:11 +0200

    Revert "all customization of the title pattern"
    This reverts commit 66a052886e1d59021843b1db8e3a7ac181d20621.

--- src/main/java/com/roomj/simpletitles/SimpleFrameTitleBuilder.java
+++ src/main/java/com/roomj/simpletitles/SimpleFrameTitleBuilder.java
@@ -7,29 +7,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class SimpleFrameTitleBuilder extends PlatformFrameTitleBuilder {
 
-  TitleSettingsState state = TitleSettingsState.getInstance();
-
-  @Override
-  public String getProjectTitle(@NotNull Project project) {
-    String title = state.projectFormat;
-    if (title.contains("{DEFAULT}")) {
-      title = title.replace("{DEFAULT}", super.getProjectTitle(project));
-    }
-    if (title.contains("{SIMPLE}")) {
-      title = title.replace("{SIMPLE}", project.getName());
-    }
-    return title;
-  }
-
   @Override
   public String getFileTitle(@NotNull Project project, @NotNull VirtualFile virtualFile) {
-    String title = state.fileFormat;
-    if (title.contains("{DEFAULT}")) {
-      title = title.replace("{DEFAULT}", super.getFileTitle(project, virtualFile));
-    }
-    if (title.contains("{SIMPLE}")) {
-      title = title.replace("{SIMPLE}", virtualFile.getName());
-    }
-    return title;
+    return virtualFile.getName();
   }
 }
